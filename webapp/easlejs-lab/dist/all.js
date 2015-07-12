80a117ccd35cebd5b6fa693a6011e2e812a26fdd
LJ <lji@tradeshift.com>
2015-07-12 17:43:03 +0800

    more animation test in easlejs lab

--- webapp/easlejs-lab/dist/all.js
+++ webapp/easlejs-lab/dist/all.js
@@ -6,8 +6,24 @@ controller('main', ['$scope', function($scope){
 		var tl = new TimelineLite();
 		tl.to(banner, 5, {callback:function(ratio){
 				banner.easleScale( ratio );
-		}, ease: Power1.easeOut});
-		
+		}, ease: Power1.easeOut, onComplete:function(){ banner.easleScaleEnd(); }
+		});
+		tl.to(banner, 4, {callback:function(ratio){
+				banner.moveLeft(ratio);
+			},
+			ease: Power1.easeOut,
+			onComplete:function(){
+				banner.moveLeftEnd();
+			}
+		});
+		tl.to(banner, 8, {callback:function(ratio){
+				banner.moveRight(ratio);
+			},
+			ease: Power1.easeOut,
+			onComplete:function(){
+				//banner.moveLeftEnd();
+			}
+		});
 	};
 }]).
 directive('dmBannerCanvas', ['$q', 'dmImgBlur',
@@ -37,6 +53,7 @@ directive('dmBannerCanvas', ['$q', 'dmImgBlur',
 			var blurRadius;
 			var scaleMatrix = new createjs.Matrix2D();
 			var scaleAnimMatrix = new createjs.Matrix2D();
+			var moveAnimMatrix = new createjs.Matrix2D();
 			
 			var stage = new createjs.Stage(canvas), eImage;
 			
@@ -66,44 +83,33 @@ directive('dmBannerCanvas', ['$q', 'dmImgBlur',
 				},
 				
 				easleScale:function(ratio){
-					console.log('oh');
 					scaleAnimMatrix.identity();
 					var sc = 1+ ratio * 0.2;
 					scaleAnimMatrix.scale(sc, sc);
 					resize();
 					
 				},
+				easleScaleEnd:function(){
+					self.bounds = eImage.getTransformedBounds();
+					self.toMoveLeft = Math.abs(self.bounds.x);
+					console.log(self.toMoveLeft);
+				},
 				
-				scale:function(ratio){
-					var ctx = canvas.getContext('2d');
-					ctx.save();
-					ctx.clearRect(0,0, canvas.width, canvas.height);
-					var img = currImage;
-					var w = img.naturalWidth, h = img.naturalHeight;
-					var canvasWidth = canvas.width, canvasHeight = canvas.height;
-					var scaleX = w/ canvasWidth; // 2/1 , 2
-					var scaleY = h/ canvasHeight; // 1/1, 1
-					
-					var scaleToCover = Math.min(scaleX, scaleY);
-					
-					var cutWidth = w - canvasWidth * scaleToCover;
-					var cutHeight = h - canvasHeight * scaleToCover;
-					var scaleRatio = 1 + 0.2 * ratio;
-					ctx.translate((canvas.width >>1), (canvas.height >>1));
-					ctx.scale(scaleRatio, scaleRatio);
-					ctx.drawImage( img, cutWidth >> 1, cutHeight >> 1, Math.round(canvasWidth * scaleToCover),
-						Math.round(canvasHeight * scaleToCover),
-					 	-(canvas.width >>1), -(canvas.height >>1), canvasWidth, canvasHeight);
-					ctx.restore();
-					
-					clipArea(layer.offsetLeft <<1, layer.offsetTop<<1,
-					layer.clientWidth <<1, layer.clientHeight<<1);
-					dmImgBlur.stackBlurCanvasRGB(layerCanvas,
-						0,0, layerCanvas.width, layerCanvas.height, self.layerBlurRadius);
+				moveLeft:function(ratio){
+					moveAnimMatrix.identity();
+					moveAnimMatrix.translate(self.toMoveLeft * ratio, 0);
+					resize();
 				},
-				move:function(){
-					
+				moveLeftEnd:function(){
+					self.bounds = eImage.getTransformedBounds();
+					self.toMoveRight = Math.abs(self.bounds.x + self.bounds.width - canvas.width);
+					console.log(self.toMoveRight);
 				},
+				moveRight:function(ratio){
+					moveAnimMatrix.identity();
+					moveAnimMatrix.translate(self.toMoveLeft - self.toMoveRight * ratio, 0);
+					resize();
+				}
 			};
 			
 			
@@ -174,13 +180,18 @@ directive('dmBannerCanvas', ['$q', 'dmImgBlur',
 				scaleMatrix.identity();
 				scaleMatrix.scale(1/scaleToCover, 1/scaleToCover);
 				
-				eImage.transformMatrix = new createjs.Matrix2D().translate(
-					canvas.width>>1, canvas.height >> 1).appendMatrix(scaleMatrix).
+				eImage.transformMatrix = new createjs.Matrix2D().
+					appendMatrix(moveAnimMatrix).
+					translate(canvas.width>>1, canvas.height >> 1).
+					appendMatrix(scaleMatrix).
 					appendMatrix(scaleAnimMatrix).
 					appendTransform(- (w >> 1), - (h >> 1), 1,1);
 				
 					
 				stage.update();
+				
+				//console.log(eImage.getTransformedBounds());
+				
 				
 				self.blur(blurRadius===undefined? attrs.blurRadius: blurRadius);
 				clipArea(layer.offsetLeft <<1, layer.offsetTop<<1,
